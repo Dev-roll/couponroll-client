52f036f3c01587f4cef22f1c99546711d73b721b
notchcoder <kokiuaxdx@gmail.com>
2023-02-19 13:21:50 +0900

    feat: QRコードスキャン機能を実装

--- app/src/main/java/app/web/couponroll/ui/capture/CaptureScreen.kt
+++ app/src/main/java/app/web/couponroll/ui/capture/CaptureScreen.kt
@@ -1,5 +1,6 @@
 package app.web.couponroll.ui.capture
 
+import android.annotation.SuppressLint
 import android.content.Context
 import android.net.Uri
 import android.util.Log
@@ -8,18 +9,19 @@ import androidx.camera.core.*
 import androidx.camera.core.ImageCapture.CAPTURE_MODE_MAXIMIZE_QUALITY
 import androidx.camera.lifecycle.ProcessCameraProvider
 import androidx.camera.view.PreviewView
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.navigationBarsPadding
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.rounded.PhotoCamera
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalLifecycleOwner
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.viewinterop.AndroidView
 import androidx.core.content.ContextCompat
 import androidx.core.net.toUri
@@ -33,6 +35,10 @@ import app.web.couponroll.ui.navigation.NavigationDestination
 import com.google.accompanist.permissions.ExperimentalPermissionsApi
 import com.google.accompanist.permissions.isGranted
 import com.google.accompanist.permissions.rememberPermissionState
+import com.google.mlkit.vision.barcode.BarcodeScannerOptions
+import com.google.mlkit.vision.barcode.BarcodeScanning
+import com.google.mlkit.vision.barcode.common.Barcode
+import com.google.mlkit.vision.common.InputImage
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
@@ -40,6 +46,7 @@ import java.io.File
 import java.net.URLEncoder
 import java.nio.charset.StandardCharsets
 import java.util.concurrent.Executor
+import java.util.concurrent.Executors
 import kotlin.coroutines.resume
 import kotlin.coroutines.resumeWithException
 import kotlin.coroutines.suspendCoroutine
@@ -122,22 +129,43 @@ fun CaptureScreen(
             Text(text = "", modifier = modifier.padding(innerPadding))
 
             Box {
+                val detectedQrCode = remember { mutableStateOf("no data") }
+
                 CameraPreview(
                     modifier = Modifier.fillMaxSize(),
-                    onUseCase = { previewUseCase = it }
-                )
-            }
-            LaunchedEffect(previewUseCase) {
-                val cameraProvider = context.getCameraProvider()
-                try {
-                    cameraProvider.unbindAll()
-                    cameraProvider.bindToLifecycle(
-                        lifecycleOwner, cameraSelector, previewUseCase, imageCaptureUseCase
+//                    onUseCase = { previewUseCase = it }
+                ) {
+                    detectedQrCode.value = it
+                }
+
+                Column(
+                    modifier = Modifier.fillMaxSize(),
+                    verticalArrangement = Arrangement.Bottom
+                ) {
+                    Text(
+                        text = detectedQrCode.value,
+                        color = Color.White,
+                        style = MaterialTheme.typography.headlineSmall,
+                        textAlign = TextAlign.Center,
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .wrapContentHeight()
+                            .background(Color.Black.copy(alpha = 0.5f))
+                            .padding(top = 20.dp, bottom = 80.dp)
                     )
-                } catch (ex: Exception) {
-                    Log.e("CameraCapture", "Failed to bind camera use cases", ex)
                 }
             }
+//            LaunchedEffect(previewUseCase) {
+//                val cameraProvider = context.getCameraProvider()
+//                try {
+//                    cameraProvider.unbindAll()
+//                    cameraProvider.bindToLifecycle(
+//                        lifecycleOwner, cameraSelector, previewUseCase, imageCaptureUseCase
+//                    )
+//                } catch (ex: Exception) {
+//                    Log.e("CameraCapture", "Failed to bind camera use cases", ex)
+//                }
+//            }
         } else {
             cameraPermissionState.launchPermissionRequest()
         }
@@ -196,8 +224,11 @@ suspend fun ImageCapture.takePicture(executor: Executor): File {
 fun CameraPreview(
     modifier: Modifier = Modifier,
     scaleType: PreviewView.ScaleType = PreviewView.ScaleType.FILL_CENTER,
-    onUseCase: (UseCase) -> Unit = { }
+    onUseCase: (UseCase) -> Unit = { },
+    onQrDetected: (code: String) -> Unit
 ) {
+    val coroutineScope = rememberCoroutineScope()
+    val lifecycleOwner = LocalLifecycleOwner.current
     AndroidView(factory = { context ->
         val previewView = PreviewView(context).apply {
             this.scaleType = scaleType
@@ -206,9 +237,69 @@ fun CameraPreview(
                 ViewGroup.LayoutParams.MATCH_PARENT
             )
         }
-        onUseCase(
-            Preview.Builder().build().also { it.setSurfaceProvider(previewView.surfaceProvider) }
-        )
+//        onUseCase(
+//            Preview.Builder().build().also { it.setSurfaceProvider(previewView.surfaceProvider) }
+//        )
+        // CameraX Preview UseCase
+        val previewUseCase = Preview.Builder()
+            .build()
+            .also {
+                it.setSurfaceProvider(previewView.surfaceProvider)
+            }
+
+        // QRコード解析UseCase
+        val qrCodeAnalysisUseCase = ImageAnalysis.Builder()
+            .setBackpressureStrategy(ImageAnalysis.STRATEGY_KEEP_ONLY_LATEST)
+            .build()
+            .also {
+                it.setAnalyzer(
+                    Executors.newSingleThreadExecutor(),
+                    QRCodeAnalyzer(onQrDetected)
+                )
+            }
+
+        coroutineScope.launch {
+            val cameraProvider = context.getCameraProvider()
+            try {
+                // use caseをライフサイクルにバインドする前にアンバインドを行う必要がある
+                cameraProvider.unbindAll()
+                cameraProvider.bindToLifecycle(
+                    lifecycleOwner, CameraSelector.DEFAULT_BACK_CAMERA, previewUseCase, qrCodeAnalysisUseCase
+                )
+            } catch (ex: Exception) {
+                Log.e("CameraPreview", "Use case binding failed", ex)
+            }
+        }
         previewView
     })
+}
+
+class QRCodeAnalyzer(private val onQrDetected: (code: String) -> Unit) : ImageAnalysis.Analyzer {
+    private val qrScannerOptions: BarcodeScannerOptions
+        get() {
+            return BarcodeScannerOptions.Builder()
+                .setBarcodeFormats(Barcode.FORMAT_QR_CODE)
+                .build()
+        }
+    private val qrScanner = BarcodeScanning.getClient(qrScannerOptions)
+
+    @SuppressLint("UnsafeOptInUsageError")
+    override fun analyze(image: ImageProxy) {
+        val mediaImage = image.image
+        // カメラから上手く画像を取得することができているとき
+        if (mediaImage != null) {
+            // CameraXで取得した画像をInputImage形式に変換する
+            val adjustedImage =
+                InputImage.fromMediaImage(mediaImage, image.imageInfo.rotationDegrees)
+
+            qrScanner.process(adjustedImage)
+                .addOnSuccessListener {
+                    if (it.isNotEmpty()) {
+                        Log.d("Success", "Detected code is ${it[0].rawValue}")
+                        onQrDetected(it[0].rawValue.toString())
+                    }
+                }
+                .addOnCompleteListener { image.close() }
+        }
+    }
 }
