72beb3ff9fff40dac0e40840440248818f289820
notchcoder <kokiuaxdx@gmail.com>
2023-02-24 01:13:01 +0900

    fix: アイコンを変更

--- app/src/main/java/app/web/couponroll/ui/navigation/TopLevelDestination.kt
+++ app/src/main/java/app/web/couponroll/ui/navigation/TopLevelDestination.kt
@@ -1,10 +1,10 @@
 package app.web.couponroll.ui.navigation
 
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.QrCodeScanner
-import androidx.compose.material.icons.filled.SpaceDashboard
-import androidx.compose.material.icons.outlined.QrCodeScanner
-import androidx.compose.material.icons.outlined.SpaceDashboard
+import androidx.compose.material.icons.filled.ConfirmationNumber
+import androidx.compose.material.icons.outlined.ConfirmationNumber
+import androidx.compose.material.icons.outlined.Storefront
+import androidx.compose.material.icons.rounded.Storefront
 import androidx.compose.ui.graphics.vector.ImageVector
 import app.web.couponroll.R
 
@@ -15,14 +15,14 @@ enum class TopLevelDestination(
     val titleTextId: Int,
 ) {
     MY_COUPONS(
-        selectedIcon = Icons.Filled.SpaceDashboard,
-        unselectedIcon = Icons.Outlined.SpaceDashboard,
+        selectedIcon = Icons.Filled.ConfirmationNumber,
+        unselectedIcon = Icons.Outlined.ConfirmationNumber,
         iconTextId = R.string.my_coupons_icon_text,
         titleTextId = R.string.my_coupons_title,
     ),
     STORE_LIST(
-        selectedIcon = Icons.Filled.QrCodeScanner,
-        unselectedIcon = Icons.Outlined.QrCodeScanner,
+        selectedIcon = Icons.Rounded.Storefront,
+        unselectedIcon = Icons.Outlined.Storefront,
         iconTextId = R.string.qr_code_scan_icon_text,
         titleTextId = R.string.store_list_title,
     ),
