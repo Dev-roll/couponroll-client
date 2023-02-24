51a2b364f7a0b49c521ae291e09349575c8c64a2
notchcoder <kokiuaxdx@gmail.com>
2023-02-24 16:19:02 +0900

    feat: クーポンのデザイン変更

--- app/src/main/java/app/web/couponroll/ui/components/CouponItem.kt
+++ app/src/main/java/app/web/couponroll/ui/components/CouponItem.kt
@@ -5,11 +5,14 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.rounded.QrCode
 import androidx.compose.material.icons.rounded.Star
 import androidx.compose.material.icons.rounded.StarBorder
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.asImageBitmap
 import androidx.compose.ui.graphics.painter.BitmapPainter
@@ -43,80 +46,133 @@ fun CouponItem(
     var isModalOpen by remember { mutableStateOf(false) }
     Box(modifier = modifier
         .clickable {
+            isModalOpen = true
 //            onTaskClick(task)
         }
         .background(color = MaterialTheme.colorScheme.secondaryContainer)
-        .padding(8.dp)
     ) {
-        Column {
+        Row(verticalAlignment = Alignment.CenterVertically) {
             Row {
-                Image(
-                    painter = rememberAsyncImagePainter(task.filePath),
-                    contentDescription = "captured image",
-                    contentScale = ContentScale.FillHeight,
-                    modifier = Modifier.size((width / 8).dp)
-                )
-                Row(
-                    modifier = modifier
-                        .padding(vertical = 16.dp),
-                ) {
-                    //                    IconToggleButton(
-                    //                        checked = task.isCompleted,
-                    //                        onCheckedChange = onCompletedChange,
-                    //                    ) {
-                    //                        Icon(
-                    //                            imageVector = if (task.isCompleted) Icons.Rounded.TaskAlt else Icons.Rounded.RadioButtonUnchecked,
-                    //                            contentDescription = if (task.isCompleted) "check on" else "check off",
-                    //                            tint = if (task.isCompleted) DoneColor else OffColor
-                    //                        )
-                    //                    }
-                    Column(
-                        modifier = Modifier
-                            .size((width / 5).dp)
-                    ) {
-                        Text(
-                            text = task.title,
-                            fontWeight = FontWeight.Bold,
-                        )
-                        Text(
-                            text = task.description,
-                            fontSize = 14.sp,
-                        )
-                    }
+                Column(horizontalAlignment = Alignment.CenterHorizontally) {
                     IconToggleButton(
-                        checked = task.isStarred,
-                        onCheckedChange = onStarredChange,
+                        checked = task.isCompleted,
+                        onCheckedChange = onCompletedChange,
+                        modifier = Modifier
+                            .padding(8.dp)
+                            .size(60.dp)
                     ) {
-                        Icon(
-                            imageVector = if (task.isStarred) Icons.Rounded.Star else Icons.Rounded.StarBorder,
-                            contentDescription = if (task.isStarred) "check on" else "check off",
-                            tint = if (task.isStarred) StarOnColor else OffColor
+                        if (task.isCompleted) {
+                            Row(
+                                verticalAlignment = Alignment.CenterVertically,
+                                horizontalArrangement = Arrangement.Center
+                            ) {
+                                Text(text = "-", color = OffColor)
+                                Spacer(modifier = Modifier.width(8.dp))
+                                Column {
+                                    Text(text = "2023", color = OffColor)
+                                    Text(text = "2.28", color = OffColor)
+                                }
+                            }
+                        } else {
+                            Row(
+                                verticalAlignment = Alignment.CenterVertically,
+                                horizontalArrangement = Arrangement.Center
+                            ) {
+                                Text(text = "-")
+                                Spacer(modifier = Modifier.width(8.dp))
+                                Column {
+                                    Text(text = "2023")
+                                    Text(text = "2.28")
+                                }
+                            }
+                        }
+                    }
+                    Box(
+                        modifier = Modifier
+                            .height(2.dp)
+                            .width(40.dp)
+                            .background(
+                                color = MaterialTheme.colorScheme.onSecondaryContainer.copy(alpha = 0.2f),
+                                shape = RoundedCornerShape(16.dp)
+                            )
+                    ) {
+                    }
+                    Icon(
+                        imageVector = Icons.Rounded.QrCode,
+                        contentDescription = null,
+                        tint = if (task.isCompleted) OffColor else MaterialTheme.colorScheme.primary,
+                        modifier = Modifier
+                            .padding(all = 18.dp)
+                            .size(40.dp)
+                    )
+                    if (isModalOpen) {
+                        AlertDialog(
+                            onDismissRequest = { isModalOpen = false },
+                            title = { Text(text = "クーポン名") },
+                            text = {
+                                Column {
+                                    Text(text = "Devroll Store")
+                                    QRCode(
+                                        text = "https://example.com",
+                                        modifier = Modifier.size(320.dp)
+                                    )
+                                }
+                            },
+                            confirmButton = {},
+                            dismissButton = {
+                                TextButton(
+                                    onClick = {
+                                        isModalOpen = false
+                                    },
+                                ) { Text(text = stringResource(id = R.string.close_button)) }
+                            }
                         )
                     }
                 }
             }
-            Button(onClick = { isModalOpen = true }) {
-                Text(text = "QRコードを表示")
+            Box(
+                modifier = Modifier
+                    .fillMaxHeight()
+                    .width(2.dp)
+                    .background(
+                        color = MaterialTheme.colorScheme.onSecondaryContainer.copy(alpha = 0.2f),
+                        shape = RoundedCornerShape(16.dp)
+                    )
+            ) {
             }
-            if (isModalOpen) {
-                AlertDialog(
-                    onDismissRequest = { isModalOpen = false },
-                    title = { Text(text = "クーポン名") },
-                    text = {
-                        Column {
-                            Text(text = "Devroll Store")
-                            QRCode(text = "https://example.com", modifier = Modifier.size(320.dp))
-                        }
-                    },
-                    confirmButton = {},
-                    dismissButton = {
-                        TextButton(
-                            onClick = {
-                                isModalOpen = false
-                            },
-                        ) { Text(text = stringResource(id = R.string.close_button)) }
-                    }
+            Column {
+                Text(
+                    text = task.title,
+                    fontWeight = FontWeight.Bold,
                 )
+                Spacer(modifier = Modifier.height(8.dp))
+                Text(
+                    text = task.description,
+                    fontSize = 14.sp,
+                )
+            }
+            Spacer(modifier = Modifier.weight(1f))
+            Box(
+                contentAlignment = Alignment.TopEnd
+            ) {
+                Image(
+                    painter = rememberAsyncImagePainter(task.filePath),
+                    contentDescription = "captured image",
+                    contentScale = ContentScale.FillHeight,
+                    modifier = Modifier
+                        .width(100.dp)
+                        .height(200.dp)
+                )
+                IconToggleButton(
+                    checked = task.isStarred,
+                    onCheckedChange = onStarredChange,
+                ) {
+                    Icon(
+                        imageVector = if (task.isStarred) Icons.Rounded.Star else Icons.Rounded.StarBorder,
+                        contentDescription = if (task.isStarred) "check on" else "check off",
+                        tint = if (task.isStarred) StarOnColor else OffColor
+                    )
+                }
             }
         }
     }
